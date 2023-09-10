a05c89b49b4d94244f760f025b78c92d13c7d033
George J <Gjohnson.pioneer@gmail.com>
2023-09-10 16:01:37 -0400

    Add user questions.

---
+++ utils/questions.js
@@ -0,0 +1,57 @@
+//Questions requiring user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project. (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+                return false;
+            }
+        }
+    },
+    {   
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project. (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions for your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Will there be an image file? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter usage information for your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
